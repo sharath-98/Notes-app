730565c505c4fbfaa57161105254762d662e84be
sharath-98 <sharathchander.p@gmail.com>
2022-10-16 12:27:10 -0500

    initial setup

---
+++ src/components/Note.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {MdDeleteForever} from 'react-icons/md'
+
+function Note() {
+  return (
+    <div className='note'>
+        <span>Hello this is first note!!!</span>
+        <div className='note-footer'>
+            <small>13/04/2021</small>
+            <MdDeleteForever className='delete-icon' size='1.3em'/>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Note
---
+++ src/components/NotesLists.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Note from './Note'
+
+function NotesLists() {
+  return (
+    <div className='notes-list'>
+        <Note/>
+        <Note/>
+        <Note/>
+        <Note/>
+    </div>
+  )
+}
+
+export default NotesLists
