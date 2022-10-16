53bd4e62f77d3989d3092ce5562d550a240b52d9
sharath-98 <sharathchander.p@gmail.com>
2022-10-16 12:41:28 -0500

    notes grid layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import './App.css';
 import NotesLists from './components/NotesLists';
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
   return (
-    <div className="App">
+    <div className="app">
+      Notes
       <NotesLists/>
     </div>
   );
--- src/components/Note.js
+++ src/components/Note.js
@@ -7,8 +7,7 @@ function Note() {
         <span>Hello this is first note!!!</span>
         <div className='note-footer'>
             <small>13/04/2021</small>
-            <MdDeleteForever className='delete-icon' size='1.3em'/>
-            
+            <MdDeleteForever className='delete-icon' size='1.3em'/>   
         </div>
     </div>
   )
