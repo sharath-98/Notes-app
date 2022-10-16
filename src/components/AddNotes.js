353a3fd77681344088148e5db47bfa346ca810f5
sharath-98 <sharathchander.p@gmail.com>
2022-10-16 13:37:14 -0500

    Whitespace and validation check on the new notes

--- src/components/AddNotes.js
+++ src/components/AddNotes.js
@@ -2,17 +2,22 @@ import React, { useState } from 'react'
 
 function AddNotes({handleAddNote}) {
 const [newNote, setNewNote] = useState("");
-
+let charCount = 200;
 const handleClick = () =>{
-    if(newNote.trim.length > 0)
+    if(newNote.trim().length > 0)
         handleAddNote(newNote);
     setNewNote("");
 }
+const handleChange = (e) =>{
+    if(e.target.value.length <= 200)
+        setNewNote(e.target.value)
+}
+
   return (
     <div className='note new'>
-        <textarea value={newNote} onChange={(e)=>setNewNote(e.target.value)} rows={8} cols={12} placeholder='Enter...'></textarea>
+        <textarea value={newNote} onChange={handleChange} rows={8} cols={12} placeholder='Enter...'></textarea>
         <div className='note-footer'>
-            <small>200 Remaining</small>
+            <small>{charCount - newNote.length} Remaining</small>
             <button className='save' onClick={handleClick}>Save</button>
         </div>
     </div>
