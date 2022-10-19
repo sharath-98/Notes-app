511ee836fa382193e1cb370273030281a653b3f9
sharath-98 <sharathchander.p@gmail.com>
2022-10-18 21:36:38 -0500

    removed margin in App

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBSmGf7Og_lQBHrnK7uf27GAY58KY2-8p0",
+  authDomain: "simple-notes-app-7e5b5.firebaseapp.com",
+  projectId: "simple-notes-app-7e5b5",
+  storageBucket: "simple-notes-app-7e5b5.appspot.com",
+  messagingSenderId: "1053208658135",
+  appId: "1:1053208658135:web:a594baaf8468102227f092",
+  measurementId: "G-8V76SN70ZC"
+};
